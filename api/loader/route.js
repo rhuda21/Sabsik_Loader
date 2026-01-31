92167a7c38fe914280d0bdc3025c7408d0c24fdd
Your Name <165597973+rhuda21@users.noreply.github.com>
2026-01-31 17:52:47 +0000

    E

--- api/loader/route.js
+++ api/loader/route.js
@@ -1,13 +1,13 @@
 export default async function handler(request, response) {
-  const GITHUB_URL = "https://raw.githubusercontent.com/TeamUBHub/UBLoader/refs/heads/main/index/Key.lua";
+  const Loader = "https://raw.githubusercontent.com/TeamUBHub/UBLoader/refs/heads/main/index/Key.lua";
   try {
-    const res = await fetch(GITHUB_URL);
+    const res = await fetch(Loader);
     if (!res.ok) {
       return response.status(res.status).send("Failed to fetch from GitHub");
     }
-    const luaContent = await res.text();
+    const c= await res.text();
     response.setHeader('Content-Type', 'text/plain');
-    return response.status(200).send(luaContent);
+    return response.status(200).send(c);
     
   } catch (error) {
     return response.status(500).json({ error: "Internal Server Error" });
